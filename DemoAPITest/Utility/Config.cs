362900e9e2aa9e2a3324b47869bb8863cbe72388
Manuel Arias <marias@planittesting.com>
2023-11-29 17:02:04 +1100

    Config file and README changes.

---
+++ DemoAPITest/Utility/Config.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAPITest.Utility
+{
+    public class Config
+    {
+        public static string getBaseUrl()
+        {
+            string json = File.ReadAllText(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.ToString() + "\\Constants.json");
+            dynamic data = JObject.Parse(json);
+
+            return data.baseUrl;
+        }
+    }
+}
